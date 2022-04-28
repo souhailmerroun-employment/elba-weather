212e9492628a824f217893b07088c4a8592f0ab0
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-28 12:07:58 +0200

    Setup react-query & get city info

--- pages/[city].tsx
+++ pages/[city].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useQuery } from "react-query";
 
 const City = () => {
     const router = useRouter()
@@ -6,9 +7,25 @@ const City = () => {
 
     console.log(city);
 
+    const API_KEY = `315a1a9065021509b2e5d8defe259136`;
+    const { isLoading, data } = useQuery('city', () =>
+        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`).then(res =>
+            res.json()
+        ),
+        {
+            enabled: !!city
+        }
+    )
+
+    console.log(data);
+
+    if (!!city) {
+        return <p>No city was detected.</p>
+    }
+
     return (
         <div>
-            <h1>City page</h1>
+            <h1>A city was detected</h1>
         </div>
     )
 }
